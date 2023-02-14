6ef9529190f50db6a29bf49379dc77d5406bcfb9
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-02-14 08:27:24 -0300

    Update and rename oi1.py to aula1.py

--- aula1.py
+++ aula1.py
@@ -1,12 +1,3 @@
-#!/usr/bin/python3
-
-import time
- 
-t = time.localtime(time.time())
-localtime = time.asctime(t)
-str = "Current Time:" + time.asctime(t)
- 
-print(str);
 print("Oi, tudo bem?")
 
 
