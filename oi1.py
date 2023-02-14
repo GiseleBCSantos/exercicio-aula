1cdab5654f2668699e5fb8d17def4b2694c33cd2
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-02-14 08:26:42 -0300

    Add files via upload

---
+++ oi1.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+
+import time
+ 
+t = time.localtime(time.time())
+localtime = time.asctime(t)
+str = "Current Time:" + time.asctime(t)
+ 
+print(str);
+print("Oi, tudo bem?")
+
+
+print("Estou estudando pyton")
+
+
+print("Uma imagem de um cachorro...")
+print(" 0____  ")
+print("  ||||  ")
+
+print('''
+ Uma imagem de um cachorro...
+ 0____
+  ||||
+''')
+
+
+print(" Meu animal favorito é um gato")
+print(" ^.^----/ ")
+print(" ---|||| ")
+ 
+print(" Eu moro em Teresina")
+
+
+
+print("    -------  ")
+print("   /-------\  ")
+print("  /---------\ ") 
+print("   |       |  ")
+print("   |[]     |  ")
+
+
+print("olá "*5)
+print("olá " + "tudo bem?")
+
+print("ha "*4)
+print("ba" + "na"*2)
+print("Bra" + "sil" + "!"*10)
+
+print("O jeito difícil...")
+print("##########")
+print("##########")
+print("##########")
+
+print("O jeito fácil...")
+print("#"*10)
+print("#"*10)
+print("#"*10)
+
+print("Desenho de onda")
+print("/\  "*10)
+print("  \/"*10)
+
+print("Aqui vai um dinossauro!")
+print('''
+               ___
+              / ._)
+       .-^^^-/  /
+     _/       _/
+    <_.|_|-|_|
+    ''')
+    
+print("E aqui vai um bolo! hummmmmmmm")
+print("O jeito difícil...")
+print(" .......... ")
+print(" iiiiiiiiii ")
+print("############")
+print("============")
+print("############")
+print("============")
+print("############")
+
+print("O jeito fácil...")
+print(" " + "."*10)
+print(" " + "i"*10)
+print("#"*12)
+print("="*12)
+print("#"*12)
+print("="*12)
+print("#"*12)
