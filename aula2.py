a05b0e349a736bd80955720913a4145333711d1e
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2023-02-16 09:46:49 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,29 @@
+#Passo 1
+print(8*2)
+#Desafio
+print(12*12.50)
+
+#Passo 2
+print(2025-1999)
+#Desafio
+print(2025-1998)
+print(2050-2023)
+
+
+#Passo 3
+print("Em que ano você nasceu?")
+nascimento = input()
+nascimento = int(nascimento)
+print("Em 2025 você terá ", 2025 - nascimento, " anos!")
+#Desafio
+ano_nascimento= int(input("Em que ano você nasceu? "))
+ano_desejado = int(input("Você deseja obter sua idade em qual ano? "))
+
+print(f"Se você nasceu em {ano_nascimento}, em {ano_desejado} você terá {ano_desejado - ano_nascimento} anos")
+
+
+idade= int(input("Qual a sua idade? "))
+print(f"Se você fosse um cachorro, teria {idade*7} anos")
+print("0____/")
+print(" ||||")
+    
